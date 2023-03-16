2abdfb0cdc7b45b19d8f238b79b67cc8efbe8060
Ahmed Sabir <43648923+ahmedssabir@users.noreply.github.com>
2023-03-16 18:59:03 +0100

    Add files via upload

--- SBERT-caption-eval/run_SDiv.sh
+++ SBERT-caption-eval/run_SDiv.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
-python SDiv_SBERT_eval.py --hyp k-5.txt
+python SDiv_SBERT_eval.py  --ref_1 ref_karpath/karpathy-refs-1.txt  --ref_2 ref_karpath/karpathy-refs-2.txt --ref_3 ref_karpath/karpathy-refs-3.txt --ref_4 ref_karpath/karpathy-refs-4.txt  --ref_5 ref_karpath/karpathy-refs-5.txt  --hyp k3_BLIP_hyp.txt --output score.txt
 
  awk -F';' '{sum+=$1; ++n} END { print "Avg: "sum"/"n"="sum/n }' < score.txt
+
