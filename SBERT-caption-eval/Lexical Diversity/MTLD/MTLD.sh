6c38f5733a70308eaa833a82f78f723478009e89
Ahmed Sabir <43648923+ahmedssabir@users.noreply.github.com>
2023-03-16 19:03:21 +0100

    Add files via upload

--- SBERT-caption-eval/Lexical Diversity/MTLD/MTLD.sh
+++ SBERT-caption-eval/Lexical Diversity/MTLD/MTLD.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
-python unique-token.py --hyp beam.txt --output score.txt 
+python MTLD.py --hyp beam.txt  --output score.txt 
 
  awk -F';' '{sum+=$1; ++n} END { print "Avg: "sum"/"n"="sum/n }' < score.txt
