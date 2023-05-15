04019c51d7b07ac664a6786927573e66f1c818d5
Ahmed Sabir <43648923+ahmedssabir@users.noreply.github.com>
2023-05-15 14:11:52 +0200

    Update model.py

--- model/model.py
+++ model/model.py
@@ -94,9 +94,8 @@ for i in range(len(get_lines(args.lm))):
     
     sim =  cosine_scores = util.pytorch_cos_sim(caption_emb, visual_context_label_emb)
     sim = sim.cpu().numpy()
-    sim = str(sim)[1:-1]
-    sim = str(sim)[1:-1]   
-
+    sim = sim.item()
+ 
 
   
     score = Visual_re_ranker(LM, visual_context_prob, sim)
